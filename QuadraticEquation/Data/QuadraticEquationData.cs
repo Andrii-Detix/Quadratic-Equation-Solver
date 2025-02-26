ca516cc6b865819766de57d2422cf04165059d0c
Andrii-Detix <aivancisin6@gmail.com>
2025-02-26 23:56:47 +0200

    Add QuadraticEquationData struct that represents coeffs and roots

---
+++ QuadraticEquation/Data/QuadraticEquationData.cs
@@ -0,0 +1,31 @@
+﻿using QuadraticEquationSolver.QuadraticEquation.Coefficients;
+using QuadraticEquationSolver.QuadraticEquation.Data.Errors;
+
+namespace QuadraticEquationSolver.QuadraticEquation.Data;
+
+public record struct QuadraticEquationData
+{
+    public QuadraticEquationData(QuadraticEquationCoefficients coefficients, IReadOnlyCollection<double> roots)
+    {
+        if (!IsValidRoots(roots)) throw new InvalidAmountOfRootsException();
+
+        Coefficients = coefficients;
+        Roots = roots;
+    }
+
+    public QuadraticEquationCoefficients Coefficients { get; }
+    public IReadOnlyCollection<double> Roots { get; }
+    public int RootCount => Roots.Count;
+
+    private bool IsValidRoots(IReadOnlyCollection<double> roots)
+    {
+        if (roots is null) return false;
+        
+        const int min = 0;
+        const int max = 2;
+        
+        int count = roots.Count;
+
+        return count >= min && count <= max;
+    }
+}
