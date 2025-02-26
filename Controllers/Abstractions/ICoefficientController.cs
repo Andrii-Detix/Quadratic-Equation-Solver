0a2d9edc036cebb1d7b3921e5b0ba5d48097b88a
Andrii-Detix <aivancisin6@gmail.com>
2025-02-27 00:41:18 +0200

    Add IDataController and ICoefficientController interfaces

---
+++ Controllers/Abstractions/ICoefficientController.cs
@@ -0,0 +1,8 @@
+﻿using QuadraticEquationSolver.QuadraticEquation.Coefficients;
+
+namespace QuadraticEquationSolver.Controllers.Abstractions;
+
+public interface ICoefficientController : IDataController<QuadraticEquationCoefficients>
+{
+    
+}
