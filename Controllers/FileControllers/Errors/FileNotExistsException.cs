e745a2c7faecf41eca05debbf17167360aa87148
Andrii-Detix <aivancisin6@gmail.com>
2025-02-27 11:53:30 +0200

    Add FileCoefficientsController to get data from file

---
+++ Controllers/FileControllers/Errors/FileNotExistsException.cs
@@ -0,0 +1,6 @@
+﻿namespace QuadraticEquationSolver.Controllers.FileControllers.Errors;
+
+public class FileNotExistsException(string path) : Exception($"File with path {path} does not exists")
+{
+    
+}
