4eeadb199f2516d528749bdd3b751a2c17327513
Andrii-Detix <aivancisin6@gmail.com>
2025-02-27 02:23:55 +0200

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using QuadraticEquationSolver.Controllers.Abstractions;
+using QuadraticEquationSolver.Controllers.InputControllers;
+using QuadraticEquationSolver.Presenters.Abstractions;
+using QuadraticEquationSolver.Presenters.ErrorPresenters;
+using QuadraticEquationSolver.Presenters.QuadraticEquationPresenter;
+using QuadraticEquationSolver.QuadraticEquation.Abstractions;
+using QuadraticEquationSolver.QuadraticEquation.Solvers;
+
+IErrorPresenter errorPresenter = new ErrorPresenter();
+
+ICoefficientsController controller = new InputCoefficientsController(errorPresenter);
+var coefficients = controller.GetData();
+
+IQuadraticEquationSolver solver = new QuadraticSolver();
+var result = solver.Solve(coefficients);
+
+IQuadraticEquationPresenter equationPresenter = new QuadraticEquationPresenter();
+equationPresenter.Present(result);
